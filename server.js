631227fe07c4e47085060d713d23d248a70cfb37
ACER@DESKTOP-D2M66R8 MINGW64 /d/Full Stack Developer <osama.moh.salem@gmail.com>
2022-07-15 01:01:12 +0300

    Crud

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(router)
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
