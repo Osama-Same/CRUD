9ec00fdeaf1ade38c007d6329d32e1877a86a81d
ACER@DESKTOP-D2M66R8 MINGW64 /d/Full Stack Developer <osama.moh.salem@gmail.com>
2022-07-29 19:39:12 +0300

    frist commit

--- server.js
+++ server.js
@@ -7,16 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use(router)
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
